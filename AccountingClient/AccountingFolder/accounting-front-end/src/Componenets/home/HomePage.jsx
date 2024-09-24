9939941f44fe3f2ddf8bcc09b530400df213ff86
Alex Baker <70116576+abaker45@users.noreply.github.com>
2024-09-23 20:31:01 -0400

    first commit

--- AccountingClient/AccountingFolder/accounting-front-end/src/Componenets/home/HomePage.jsx
+++ AccountingClient/AccountingFolder/accounting-front-end/src/Componenets/home/HomePage.jsx
@@ -9,6 +9,7 @@ const HomePage = () => {
                 <h1>This this the home page.. more incomming</h1>
                 <a href="/DisplayUserList">Click here to see the Full User List</a>
                 <a href="/#">Click here to edit users?</a>
+                {/* alex was here */}
             </form>
 
         </div>
